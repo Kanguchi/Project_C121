68a17e8b29b109dc331802502b7ad8b617fb2ca1
Kanguchi <70295418+Kanguchi@users.noreply.github.com>
2021-06-08 16:37:14 -0400

    Add files via upload

---
+++ blackscreen.py
@@ -0,0 +1,31 @@
+import time, cv2
+import numpy as np
+
+video = cv2.VideoCapture(0)
+image = cv2.imread("background.jpeg")
+
+while True:
+    ret, frame = video.read()
+
+    frame = cv2.resize(frame, (640, 480))
+    image = cv2.resize(image, (640, 480))
+
+    l_black = np.array([30, 30, 0])
+    u_black = np.array([104, 157, 70])
+
+    mask = cv2.inRange(frame, l_black, u_black)
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    f = frame - res
+    f = np.where(f == 0, image, f)
+
+    cv2.imshow('Video', frame)
+    cv2.imshow('Mask', f)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+  
+video.release() 
+cv2.destroyAllWindows() 
+
+
